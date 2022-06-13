4e445576eb4e5142256d74f4ac2d3c07184dcf30
Ionut Vasile <email@iovasile.dev>
2022-06-14 01:58:30 +0300

    config: prisma + planetscale

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import { prisma } from "../db/client";
 
-const Home: NextPage = () => {
+const Home: NextPage = (props: any) => {
   return (
     <div>
       <Head>
@@ -12,12 +12,23 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <h1>
+        <h1 className="text-5xl font-bold">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <code>{props.questions}</code>
       </main>
     </div>
   );
 };
 
 export default Home;
+
+export const getStaticProps = async () => {
+  const questions = await prisma.pollQuestion.findMany();
+
+  return {
+    props: {
+      questions: JSON.stringify(questions),
+    },
+  };
+};
